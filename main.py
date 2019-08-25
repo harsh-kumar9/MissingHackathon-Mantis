79e373f3fa8c8d92927ffc4f0d8548b625d84e60
harsh-kumar9 <33204114+harsh-kumar9@users.noreply.github.com>
2019-08-25 17:33:07 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,249 @@
+from djitellopy import Tello
+import cv2
+import pygame
+from pygame.locals import *
+import numpy as np
+import time
+from face_detect import find_faces
+from image_commons import nparray_as_image, draw_with_alpha
+import os
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read('trainer/trainer.yml')
+cascadePath = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascadePath)
+
+names = ['None', 'Hostile', 'Victim #2', 'Victim #3', 'Victim #4', 'W']
+
+# Speed of the drone
+S = 60
+# Frames per second of the pygame window display
+FPS = 25
+
+classNames = {0: 'background',
+              1: 'person', 2: 'bicycle', 3: 'car', 4: 'motorcycle', 5: 'airplane', 6: 'bus',
+              7: 'train', 8: 'truck', 9: 'boat', 10: 'traffic light', 11: 'fire hydrant',
+              13: 'stop sign', 14: 'parking meter', 15: 'bench', 16: 'bird', 17: 'cat',
+              18: 'dog', 19: 'horse', 20: 'sheep', 21: 'cow', 22: 'elephant', 23: 'bear',
+              24: 'zebra', 25: 'giraffe', 27: 'backpack', 28: 'umbrella', 31: 'handbag',
+              32: 'tie', 33: 'suitcase', 34: 'frisbee', 35: 'skis', 36: 'snowboard',
+              37: 'sports ball', 38: 'kite', 39: 'baseball bat', 40: 'baseball glove',
+              41: 'skateboard', 42: 'surfboard', 43: 'tennis racket', 44: 'bottle',
+              46: 'wine glass', 47: 'cup', 48: 'fork', 49: 'knife', 50: 'spoon',
+              51: 'bowl', 52: 'banana', 53: 'apple', 54: 'sandwich', 55: 'orange',
+              56: 'broccoli', 57: 'carrot', 58: 'hot dog', 59: 'pizza', 60: 'donut',
+              61: 'cake', 62: 'chair', 63: 'couch', 64: 'potted plant', 65: 'bed',
+              67: 'dining table', 70: 'toilet', 72: 'tv', 73: 'laptop', 74: 'mouse',
+              75: 'remote', 76: 'keyboard', 77: 'cell phone', 78: 'microwave', 79: 'oven',
+              80: 'toaster', 81: 'sink', 82: 'refrigerator', 84: 'book', 85: 'clock',
+              86: 'vase', 87: 'scissors', 88: 'teddy bear', 89: 'hair drier', 90: 'toothbrush'}
+
+
+def id_class_name(class_id, classes):
+    for key, value in classes.items():
+        if class_id == key:
+            return value
+
+
+# Loading model
+model = cv2.dnn.readNetFromTensorflow('models/frozen_inference_graph.pb',
+                                      'models/ssd_mobilenet_v2_coco_2018_03_29.pbtxt')
+class FrontEnd(object):
+    """ Maintains the Tello display and moves it through the keyboard keys.
+        Press escape key to quit.
+        The controls are:
+            - T: Takeoff
+            - L: Land
+            - Arrow keys: Forward, backward, left and right.
+            - A and D: Counter clockwise and clockwise rotations
+            - W and S: Up and down.
+    """
+
+    def __init__(self):
+        # Init pygame
+        pygame.init()
+
+        # Creat pygame window
+        pygame.display.set_caption("Tello video stream")
+        self.screen = pygame.display.set_mode([960, 720])
+
+        # Init Tello object that interacts with the Tello drone
+        self.tello = Tello()
+
+        # Drone velocities between -100~100
+        self.for_back_velocity = 0
+        self.left_right_velocity = 0
+        self.up_down_velocity = 0
+        self.yaw_velocity = 0
+        self.speed = 10
+
+        self.send_rc_control = False
+
+        # create update timer
+        pygame.time.set_timer(USEREVENT + 1, 50)
+
+    def run(self):
+
+        if not self.tello.connect():
+            print("Tello not connected")
+            return
+
+        if not self.tello.set_speed(self.speed):
+            print("Not set speed to lowest possible")
+            return
+
+        # In case streaming is on. This happens when we quit this program without the escape key.
+        if not self.tello.streamoff():
+            print("Could not stop video stream")
+            return
+
+        if not self.tello.streamon():
+            print("Could not start video stream")
+            return
+
+        frame_read = self.tello.get_frame_read()
+        # cam = self.tello.get_video_capture()
+        should_stop = False
+        while not should_stop:
+
+            for event in pygame.event.get():
+                if event.type == USEREVENT + 1:
+                    self.update()
+                elif event.type == QUIT:
+                    should_stop = True
+                elif event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        should_stop = True
+                    else:
+                        self.keydown(event.key)
+                elif event.type == KEYUP:
+                    self.keyup(event.key)
+
+            if frame_read.stopped:
+                frame_read.stop()
+                break
+            # ret, image = cam.read()
+            image = frame_read.frame
+            image_height, image_width, _ = image.shape
+
+            model.setInput(cv2.dnn.blobFromImage(image, size=(300, 300), swapRB=True))
+            output = model.forward()
+            # print(output[0,0,:,:].shape)
+
+
+            for detection in output[0, 0, :, :]:
+                confidence = detection[2]
+                if confidence > .5:
+                    class_id = detection[1]
+                    class_name=id_class_name(class_id,classNames)
+                    print(str(str(class_id) + " " + str(detection[2])  + " " + class_name))
+                    box_x = detection[3] * image_width
+                    box_y = detection[4] * image_height
+                    box_width = detection[5] * image_width
+                    box_height = detection[6] * image_height
+                    cv2.rectangle(image, (int(box_x), int(box_y)), (int(box_width), int(box_height)), (23, 230, 210), thickness=10)
+                    cv2.putText(image,class_name ,(int(box_x), int(box_y+.05*image_height)),cv2.FONT_HERSHEY_SIMPLEX,(.005*image_width),(0, 0, 255))
+
+            gray = cv2.cvtColor((image),cv2.COLOR_BGR2GRAY)
+            faces = faceCascade.detectMultiScale( 
+            gray,
+            scaleFactor = 1.2,
+            minNeighbors = 5,
+            minSize = (int(96), int(72)),
+            )
+            for(x,y,w,h) in faces:
+
+                cv2.rectangle(image, (x,y), (x+w,y+h), (0,255,0), 2)
+    
+                id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
+                #faceValue = round(100 - int(confidence))
+                # Check if confidence is less them 100 ==> "0" is perfect match 
+                if (confidence < 100):
+                    id = names[id]
+                    faceValue = 100 - int(confidence)
+                    #print(faceValue)
+                    confidence = "  {0}%".format(round(100 - confidence))
+                else:
+                    id = "unknown"
+                    faceValue = 100 - int(confidence)
+                    #print(faceValue)
+                    confidence = "  {0}%".format(round(100 - confidence))
+                
+                cv2.putText(image, str(id), (x+5,y-5), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
+                cv2.putText(image, str(confidence), (x+5,y+h-5), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,0), 1)
+
+
+
+            frame = image
+            # cv2.imshow('camera', image)
+            self.screen.fill([0, 0, 0])
+            frame = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            frame = np.rot90(frame)
+            frame = np.flipud(frame)
+            frame = pygame.surfarray.make_surface(frame)
+            self.screen.blit(frame, (0, 0))
+            pygame.display.update()
+
+            time.sleep(1 / FPS)
+
+        # Call it always before finishing. I deallocate resources.
+        self.tello.end()
+
+    def keydown(self, key):
+        """ Update velocities based on key pressed
+        Arguments:
+            key: pygame key
+        """
+        if key == pygame.K_UP:  # set forward velocity
+            self.for_back_velocity = S
+        elif key == pygame.K_DOWN:  # set backward velocity
+            self.for_back_velocity = -S
+        elif key == pygame.K_LEFT:  # set left velocity
+            self.left_right_velocity = -S
+        elif key == pygame.K_RIGHT:  # set right velocity
+            self.left_right_velocity = S
+        elif key == pygame.K_w:  # set up velocity
+            self.up_down_velocity = S
+        elif key == pygame.K_s:  # set down velocity
+            self.up_down_velocity = -S
+        elif key == pygame.K_a:  # set yaw clockwise velocity
+            self.yaw_velocity = -S
+        elif key == pygame.K_d:  # set yaw counter clockwise velocity
+            self.yaw_velocity = S
+
+    def keyup(self, key):
+        """ Update velocities based on key released
+        Arguments:
+            key: pygame key
+        """
+        if key == pygame.K_UP or key == pygame.K_DOWN:  # set zero forward/backward velocity
+            self.for_back_velocity = 0
+        elif key == pygame.K_LEFT or key == pygame.K_RIGHT:  # set zero left/right velocity
+            self.left_right_velocity = 0
+        elif key == pygame.K_w or key == pygame.K_s:  # set zero up/down velocity
+            self.up_down_velocity = 0
+        elif key == pygame.K_a or key == pygame.K_d:  # set zero yaw velocity
+            self.yaw_velocity = 0
+        elif key == pygame.K_t:  # takeoff
+            self.tello.takeoff()
+            self.send_rc_control = True
+        elif key == pygame.K_l:  # land
+            self.tello.land()
+            self.send_rc_control = False
+
+    def update(self):
+        """ Update routine. Send velocities to Tello."""
+        if self.send_rc_control:
+            self.tello.send_rc_control(self.left_right_velocity, self.for_back_velocity, self.up_down_velocity,
+                                       self.yaw_velocity)
+
+
+def main():
+    frontend = FrontEnd()
+
+    # run frontend
+    frontend.run()
+
+
+if __name__ == '__main__':
+    main()
